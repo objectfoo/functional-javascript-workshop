0083444499087bd59d0d30fa15c843eef1c66b41
Satake <satake@objectfoo.com>
2014-03-30 22:45:20 -0700

    initial

---
+++ doubleall.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function doubleAll(numbers) {
+	return numbers.map(function double(num) {
+		return num * 2;
+	});
+}
+
+module.exports = doubleAll;
---
+++ higherorderfunctions.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function repeat(operation, num) {
+	
+	if (num > 0) {
+		operation();
+		return repeat(operation, --num);
+	}
+}
+
+module.exports = repeat;
